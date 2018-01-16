b5d97208e9dfea920af4e4ec204b2ed03571fe40
tarolinus <madej.mario@gmail.com>
2018-01-16 16:23:01 +0100

    fixed typo

--- app/actions/index.js
+++ app/actions/index.js
@@ -17,7 +17,7 @@ export function getData(){
         //delay the retrieval [Sample reasons only]
         setTimeout(() => {
             var data  = Data.instructions;
-            dispatch({type: DATA_AVAILABLE, data:data});
+            dispatch({type: DATA_AVAILABLE, data: data});
         }, 2000);
 
     };
